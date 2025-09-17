acc704251614185f763fe18d29029c949c357d36
MaimunaSabir <maimunasabir13@gmail.com>
2025-09-17 18:00:23 +0500

    Update reverseArray.java

--- reverseArray.java
+++ reverseArray.java
@@ -2,13 +2,13 @@ public class Main {
 	public static void main(String[] args) {
 	
 			int array []={2,4,1,7,9};
-		
+		int end=array.length-1;
 for(int i=0;i<array.length/2;i++){
 	 
 	 int temp=array[i];
-	 array[i]=array[array.length-1-i];
-	 array[array.length-1-i]=temp;
-	 
+	 array[i]=array[end];
+	 array[end]=temp;
+	 end--;
 }
 	 	 	 		
 for(int i=0;i<array.length;i++){

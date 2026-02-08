aa26b6823e0ad0b2e8237efd388485ded4572a43
MaimunaSabir <maimunasabir13@gmail.com>
2026-02-08 17:22:09 +0500

    Add files via upload

---
+++ sumOf2Variable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sumOf2Variable{
+    public static void main(String arg []){
+
+        System.out.println("Enter 2 numbers :");
+        Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+              int b=sc.nextInt();
+              int sum=a+b;
+        System.out.println("SUM :"+sum);
+sc.close();
+
+    }
+}

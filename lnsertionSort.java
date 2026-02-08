c05051853a2b56eaaa850f30c8896769f98da3e4
MaimunaSabir <maimunasabir13@gmail.com>
2026-02-08 19:25:50 +0500

    Add files via upload

---
+++ lnsertionSort.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {8, 5, 3, 7, 1, 2};
+
+        for(int i = 1; i < arr.length; i++) {		
+            int curr = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > curr) {
+                arr[j + 1] = arr[j];		
+                j--;
+            }
+            arr[j + 1] = curr;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+    }
+}
